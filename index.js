3a820fb0cf8ce00c4f02df800563580efafc2d3a
Mandal-Arpan2002 <100986138+Arpaninformationtech@users.noreply.github.com>
2022-07-17 11:02:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var randomNumber1 = Math.floor(1 + (Math.random()*6));
 var randomNumber2 = Math.floor(1 + (Math.random()*6));
-//document.querySelector("img.img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
-//document.querySelector("img.img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
-document.querySelectorAll("img")[0].setAttribute("src", "images/dice" + randomNumber1 + ".png");
-document.querySelectorAll("img")[1].setAttribute("src", "images/dice" + randomNumber2 + ".png");
+//document.querySelector("img.img1").setAttribute("src", "dice" + randomNumber1 + ".png");
+//document.querySelector("img.img2").setAttribute("src", "dice" + randomNumber2 + ".png");
+document.querySelectorAll("img")[0].setAttribute("src", "dice" + randomNumber1 + ".png");
+document.querySelectorAll("img")[1].setAttribute("src", "dice" + randomNumber2 + ".png");
 
 if (randomNumber1 > randomNumber2)
 {
